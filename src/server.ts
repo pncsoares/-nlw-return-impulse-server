b01abe2010267a7693fe0d527bb54979f865e339
Pedro Soares <pncsoares@gmail.com>
2022-05-06 16:38:53 +0100

    Add port to environment variable

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ app.use(cors({
 app.use(express.json());
 app.use(routes);
 
-const port = 3333;
+const port = process.env.PORT || 3333;
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
